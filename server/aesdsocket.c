07ea02020dd2fd6880cb2d3d7aa9923931d8db46
Hamzeh <hamzeh.nasajpour@gmail.com>
2023-10-02 10:12:15 +0100

    minor modification in socket app

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -77,7 +77,7 @@ int main(int argc, char **argv)
     struct addrinfo* addr_info = NULL;
     struct addrinfo hints;
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
